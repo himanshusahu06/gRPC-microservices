ad72a1a68414aa7fdbdda4f443230a12501f6088
Himanshu <himanshusahu24@gmail.com>
2020-05-25 20:20:15 +0530

    [server steaminng gRPC] greet many times server push gRPC

--- src/greet/greet_client/client.go
+++ src/greet/greet_client/client.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"greet/greetpb"
+	"io"
 	"log"
 
 	"google.golang.org/grpc"
@@ -20,9 +21,11 @@ func main() {
 	c := greetpb.NewGreetServiceClient(conn)
 	//fmt.Printf("Created client: %f\n", c)
 	doUnary(c)
+	doServerStreaming(c)
 }
 
 func doUnary(c greetpb.GreetServiceClient) {
+	fmt.Println("Starting unary RPC client..")
 	// invoking RPC
 	greetRequest := &greetpb.GreetingRequest{
 		Greeting: &greetpb.Greeting{
@@ -35,4 +38,31 @@ func doUnary(c greetpb.GreetServiceClient) {
 		log.Fatalf("error invoking greet RPC: %v", err)
 	}
 	fmt.Printf("greet response %v", greetResponse)
+}
+
+func doServerStreaming(c greetpb.GreetServiceClient) {
+	fmt.Println("Starting server stream RPC client..")
+	greetRequest := &greetpb.GreetManyTimesRequest{
+		Greeting: &greetpb.Greeting{
+			FirstName: "Himanshu",
+			LastName:  "Sahu",
+		},
+	}
+	greetStreamResponse, err := c.GreetManyTimes(context.Background(), greetRequest)
+	if err != nil {
+		log.Fatalf("error invoking greet many times RPC: %v", err)
+	}
+
+	for {
+		msg, err := greetStreamResponse.Recv()
+		// when stream ends, client will get EOF
+		if err == io.EOF {
+			// when stream ends, client will get EOF
+			break
+		}
+		if err != nil {
+			log.Fatalf("Error while reading stream: %v\n", err)
+		}
+		fmt.Println(msg)
+	}
 }
--- src/greet/greet_server/server.go
+++ src/greet/greet_server/server.go
@@ -6,13 +6,16 @@ import (
 	"greet/greetpb"
 	"log"
 	"net"
+	"strconv"
+	"time"
 
 	"google.golang.org/grpc"
 )
 
+// struct that will implement gRPC interfaces
 type server struct{}
 
-// RPC implementation
+// Greet is unary API
 func (*server) Greet(ctx context.Context, req *greetpb.GreetingRequest) (*greetpb.GreetingResponse, error) {
 	fmt.Printf("Greet RPC was invoked with %v\n", req)
 	firstName := req.GetGreeting().GetFirstName()
@@ -21,6 +24,23 @@ func (*server) Greet(ctx context.Context, req *greetpb.GreetingRequest) (*greetp
 	return &greetpb.GreetingResponse{
 		Result: result,
 	}, nil
+}
+
+// GreetManyTimes is streaming gRPC
+func (*server) GreetManyTimes(req *greetpb.GreetManyTimesRequest, stream greetpb.GreetService_GreetManyTimesServer) error {
+	fmt.Printf("Greet many times RPC was invoked with %v\n", req)
+	firstName := req.GetGreeting().GetFirstName()
+	lastName := req.GetGreeting().GetLastName()
+	// just greet for 10 times
+	for i := 0; i < 10; i++ {
+		res := &greetpb.GreetManyTimesResponse{
+			Result: "Hello " + firstName + " " + lastName + " " + strconv.Itoa(i),
+		}
+		stream.Send(res)
+		time.Sleep(1000 * time.Millisecond)
+	}
+	fmt.Println("All data has been streamed successfully.")
+	return nil
 }
 
 func main() {
